4e87b4c769285230a09e00366fff7fd19c28840f
Israel Fonseca Zárate <ifonsecap351@gmail.com>
2025-06-15 22:34:12 -0600

    permissions filesddd

--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "[permissions.sh] Running as: $(whoami)" >> /tmp/deploy_debug.log
+ls -l /home/ec2-user/flask-app/scripts >> /tmp/deploy_debug.log
+
 chmod +x /home/ec2-user/flask-app/scripts/*.sh
+echo "[permissions.sh] chmod done" >> /tmp/deploy_debug.log
+
+ls -l /home/ec2-user/flask-app/scripts >> /tmp/deploy_debug.log
